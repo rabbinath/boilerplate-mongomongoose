46b23353bd44557ad086f17d8d78e23433d94151
Rabindra Nath <rabbinath@gmail.com>
2021-11-25 20:21:24 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 //</Your>mongoose.connect(process.env.MONGO_URI);
